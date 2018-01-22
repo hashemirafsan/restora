2a771d7e34a32940e27127d50e47506bd525732b
Hashemi Rafsan <rafsanhashemi@gmail.com>
2018-01-22 14:13:04 +0600

    add marker

--- src/pages/select-location/select-location.ts
+++ src/pages/select-location/select-location.ts
@@ -1,6 +1,17 @@
 import { Component, ViewChild, ElementRef } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { Geolocation } from '@ionic-native/geolocation';
+import {
+ GoogleMaps,
+ GoogleMap,
+ GoogleMapsEvent,
+ GoogleMapOptions,
+ CameraPosition,
+ MarkerOptions,
+ Marker
+} from '@ionic-native/google-maps';
+import axios from 'axios';
+import { route } from '../../assets/Auth/Auth';
 
 declare var google;
 
@@ -12,6 +23,8 @@ export class SelectLocationPage {
 
   @ViewChild('map') mapElement: ElementRef;
   map: any;
+  lastLat: 0;
+  lastLng: 0;
 
   constructor(public navCtrl: NavController, public navParams: NavParams,public geolocation: Geolocation) {
 
@@ -30,10 +43,28 @@ export class SelectLocationPage {
       let mapOptions = {
         center: latLng,
         zoom: 15,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        draggable: true
       }
  
       this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+
+      let marker = new google.maps.Marker({
+        map: this.map,
+        animation: google.maps.Animation.DROP,
+        position: latLng,
+        draggable: true
+      });
+
+      google.maps.event.addListener(marker, 'dragend', () =>{ 
+         this.lastLat = marker.position.lat()
+         this.lastLng = marker.position.lng()
+      
+      });
+      
+
+      console.log(this.map)
+
  
     }, (err) => {
       console.log(err);
@@ -42,22 +73,19 @@ export class SelectLocationPage {
   }
 
   addMarker(){
-    
-    this.geolocation.getCurrentPosition().then((position) => {
-        let latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-    
 
-        let marker = new google.maps.Marker({
-          map: this.map,
-          animation: google.maps.Animation.DROP,
-          position: latLng
-        });
-       
-        let content = "<h4>Information!</h4>";         
-       
-        this.addInfoWindow(marker, content);
-
-    });
+    let data = {
+      lat: this.lastLat,
+      lng: this.lastLng
+    }
+    
+    axios.post(route.app_url + '/store-lat-lng/' + this.navParams.data.restaurant_id, data)
+         .then((response) => {
+            console.log(response)
+         })
+         .catch((err) => {
+           console.log(err)
+         })
    
   }
 
